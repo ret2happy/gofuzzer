3e77164336955bcd5743a869318127a8e1a4b71a
HAPPY <ret2happy@126.com>
2022-07-12 16:21:06 +0800

    add consume function

--- gofuzzer.go
+++ gofuzzer.go
@@ -1,9 +1,11 @@
 package gofuzzer
 
 import (
+	"encoding/binary"
 	"fmt"
 	"github.com/ret2happy/gofuzzer/utils"
 	"io/ioutil"
+	"math/big"
 	"os"
 	"path/filepath"
 	"testing"
@@ -38,4 +40,53 @@ func DumpFuzzCoreCoverage(t *testing.T, corpusPath string, callback FuzzCoreFunc
 		}
 	}
 	println("All corpus files processed.")
+}
+
+func ConsumeBool(cursor *int, fuzzData []byte) (bool, error) {
+	bakCursor := *cursor
+	*cursor += 1
+	if bakCursor >= len(fuzzData) {
+		return false, fmt.Errorf("exhausted all fuzz data")
+	}
+	return fuzzData[bakCursor]%2 == 0, nil
+}
+
+func ConsumeUint8Range(cursor *int, fuzzData []byte, maxNum uint8) (uint8, error) {
+	bakCursor := *cursor
+	*cursor += 1
+	if bakCursor >= len(fuzzData) {
+		*cursor = len(fuzzData)
+		return 0, fmt.Errorf("exhausted all fuzz data")
+	}
+	return fuzzData[bakCursor] % maxNum, nil
+}
+
+func ConsumeUint64(cursor *int, fuzzData []byte) (uint64, error) {
+	bakCursor := *cursor
+	byteLength := 8
+	if bakCursor >= len(fuzzData) || len(fuzzData)-bakCursor < byteLength {
+		*cursor = len(fuzzData)
+		return 0, fmt.Errorf("exhausted all fuzz data")
+	}
+	*cursor += byteLength
+	return binary.LittleEndian.Uint64(fuzzData[bakCursor : bakCursor+byteLength]), nil
+}
+
+func ConsumeInt64(cursor *int, fuzzData []byte) (int64, error) {
+	result, err := ConsumeUint64(cursor, fuzzData)
+	return int64(result), err
+}
+
+func ConsumeBigInt(cursor *int, fuzzData []byte, bitLength uint) (big.Int, error) {
+	bakCursor := *cursor
+	//byteLength := 32
+	targetByteLength := int(bitLength / 8)
+	var result big.Int
+	if bakCursor >= len(fuzzData) || bakCursor+targetByteLength > len(fuzzData) || targetByteLength > 32 {
+		*cursor = len(fuzzData)
+		return result, fmt.Errorf("exhausted all fuzz data")
+	}
+	*cursor += targetByteLength
+	result.SetBytes(fuzzData[bakCursor : bakCursor+targetByteLength])
+	return result, nil
 }
